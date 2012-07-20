9471f3e354ca1f04d81313ffcd040d228db034f1
Sebastian Marro <smarro@thymbra.com>
2012-07-20 19:08:21 -0300

    Change copyright notice files and README support information

--- __tryton__.py
+++ __tryton__.py
@@ -1,5 +1,6 @@
-#This file is part of Tryton.  The COPYRIGHT file at the top level of
-#this repository contains the full copyright notices and license terms.
+#This file is part of the account_ar module for Tryton.
+#The COPYRIGHT file at the top level of this repository contains
+#the full copyright notices and license terms.
 {
     'name': 'Account Argentina',
     'name_es_AR': 'Plan de cuentas Argentino Demo',
