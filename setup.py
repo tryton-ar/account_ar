22fda75b34c53a9f884ac9ce59fdcbf8782815b8
Bruno M. Villasanti <bvillasanti@thymbra.com>
2014-02-09 00:22:39 -0300

    Simplify module information with python configuration

--- setup.py
+++ setup.py
@@ -8,47 +8,45 @@ import re
 import os
 import ConfigParser
 
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 config = ConfigParser.ConfigParser()
 config.readfp(open('tryton.cfg'))
 info = dict(config.items('tryton'))
-
 for key in ('depends', 'extras_depend', 'xml'):
     if key in info:
         info[key] = info[key].strip().splitlines()
-
 major_version, minor_version, _ = info.get('version', '0.0.1').split('.', 2)
 major_version = int(major_version)
 minor_version = int(minor_version)
 
-#Third party (no tryton modules, required)
 requires = []
 for dep in info.get('depends', []):
     if not re.match(r'(ir|res|webdav)(\W|$)', dep):
         requires.append('trytond_%s >= %s.%s, < %s.%s' %
                 (dep, major_version, minor_version, major_version,
                     minor_version + 1))
-
 requires.append('trytond >= %s.%s, < %s.%s' %
         (major_version, minor_version, major_version, minor_version + 1))
 
 setup(name='trytond_account_ar',
     version=info.get('version', '0.0.1'),
-    description=info.get('description', ''),
-    author=info.get('author', ''),
-    author_email=info.get('email', ''),
-    url=info.get('website', ''),
+    description='Tryton module to add an account chart template for Argentina',
+    long_description=read('README'),
+    author='Thymbra',
+    author_email='info@thymbra.com',
+    url='http://www.thymbra.com/',
     package_dir={'trytond.modules.account_ar': '.'},
     packages=[
         'trytond.modules.account_ar',
-    ],
+        ],
     package_data={
-        'trytond.modules.account_ar': (info.get('xml', []) \
-                + ['tryton.cfg', 'view/*.xml', 'locale/*.po', '*.odt',
-                    'icons/*.svg']),
-    },
+        'trytond.modules.account_ar': (info.get('xml', [])
+            + ['tryton.cfg', 'view/*.xml', 'locale/*.po', '*.odt',
+                'icons/*.svg']),
+        },
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Plugins',
@@ -64,7 +62,7 @@ setup(name='trytond_account_ar',
         'Programming Language :: Python :: 2.7',
         'Topic :: Office/Business',
         'Topic :: Office/Business :: Financial :: Accounting',
-    ],
+        ],
     license='GPL-3',
     install_requires=requires,
     zip_safe=False,
@@ -72,4 +70,4 @@ setup(name='trytond_account_ar',
     [trytond.modules]
     account_ar = trytond.modules.account_ar
     """,
-)
+    )
