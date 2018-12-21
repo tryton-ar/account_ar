6b40666299cbdaea0b833eb7b7cbce981b147975
Adrián Bernardi <adrian.bernardi@gmail.com>
2018-12-21 20:54:57 -0300

    Add tests suite

---
+++ tests/__init__.py
@@ -0,0 +1,9 @@
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+
+try:
+    from trytond.modules.account_ar.tests.test_account_ar import suite
+except ImportError:
+    from .test_account_ar import suite
+
+__all__ = ['suite']
