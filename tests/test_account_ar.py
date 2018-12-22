b2f9e5cf1766c0ee1e607b0a5eab1bbd2fe39c41
Adrián Bernardi <adrian.bernardi@gmail.com>
2018-12-21 22:31:18 -0300

    Update tests suite

--- tests/test_account_ar.py
+++ tests/test_account_ar.py
@@ -2,8 +2,8 @@
 # this repository contains the full copyright notices and license terms.
 import unittest
 
-import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
+from trytond.tests.test_tryton import suite as test_suite
 
 
 class AccountTestCase(ModuleTestCase):
@@ -12,7 +12,7 @@ class AccountTestCase(ModuleTestCase):
 
 
 def suite():
-    suite = trytond.tests.test_tryton.suite()
+    suite = test_suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-        AccountTestCase))
+            AccountTestCase))
     return suite
