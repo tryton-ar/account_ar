e8d19adca49afe289a731e9206b72401a17d59a2
Adrián Bernardi <adrian@silix.com.ar>
2025-08-08 19:52:16 -0300

    Prepare release 7.0.1

--- __init__.py
+++ __init__.py
@@ -3,11 +3,11 @@
 # the full copyright notices and license terms.
 
 from trytond.pool import Pool
-
 from . import account
 from . import statement
 
 __all__ = ['register']
+
 
 def register():
     Pool.register(
