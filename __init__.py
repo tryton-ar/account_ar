4359fe9e650f46511c39fbb47e3b7cb1c49128cf
Luciano Rossi <lukio@gcoop.coop>
2026-05-31 18:21:00 -0300

    Use  to register in the Pool

--- __init__.py
+++ __init__.py
@@ -1,33 +1,3 @@
 # This file is part of the account_ar module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-
-from trytond.pool import Pool
-from . import account
-from . import move
-from . import fiscalyear
-from . import statement
-
-__all__ = ['register']
-
-
-def register():
-    Pool.register(
-        account.TaxTemplate,
-        account.Tax,
-        account.TaxGroup,
-        account.Account,
-        move.Line,
-        fiscalyear.BalanceDeferralStart,
-        fiscalyear.RestartDeferralStart,
-        module='account_ar', type_='model')
-    Pool.register(
-        statement.Line,
-        statement.Statement,
-        statement.StatementLine,
-        module='account_ar', type_='model',
-        depends=['account_statement'])
-    Pool.register(
-        fiscalyear.BalanceDeferral,
-        fiscalyear.RestartDeferral,
-        module='account_ar', type_='wizard')
