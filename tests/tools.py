ffb444da9e9ee4de417621bf1d42a2e82acd7255
Luciano Rossi <lukio@gcoop.coop>
2026-07-08 18:18:02 -0300

    Move get_tax and get_tax_group helpers to account_ar.tests.tools

--- tests/tools.py
+++ tests/tools.py
@@ -5,7 +5,7 @@
 from proteus import Model
 
 
-__all__ = ['get_tax', ]
+__all__ = ['get_tax', 'get_tax_group']
 
 
 def get_tax(name='IVA Ventas 21%', config=None):
@@ -17,6 +17,19 @@ def get_tax(name='IVA Ventas 21%', config=None):
         ])
 
     return tax
+
+
+def get_tax_group(code='IVA', kind='sale', afip_kind='gravado', config=None):
+    "Return tax group"
+    TaxGroup = Model.get('account.tax.group', config=config)
+
+    group, = TaxGroup.find([
+        ('code', '=', code),
+        ('kind', '=', kind),
+        ('afip_kind', '=', afip_kind),
+        ])
+
+    return group
 
 
 def get_accounts(company, config=None):
